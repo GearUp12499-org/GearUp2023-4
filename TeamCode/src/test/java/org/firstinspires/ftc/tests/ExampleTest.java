ea4288ae9341cd227417a79d17881f43b6c03d68
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2023-09-10 19:04:58 -0700

    Add JUnit tests. Click to expand ->
    Use the Tests view (in the file explorer, dropdown with label Android) to show unit tests.
    Annotate test methods with @Test.
    To run tests, create a "JUnit" configuration. See me for assistance.

---
+++ TeamCode/src/test/java/org/firstinspires/ftc/tests/ExampleTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.tests;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void add() {
+        assertEquals(2, 1 + 1);
+    }
+}
