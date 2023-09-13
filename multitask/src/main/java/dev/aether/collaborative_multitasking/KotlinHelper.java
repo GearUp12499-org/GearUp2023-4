604359d26978f5362ae89fc25fa1d915a98685ad
PenguinEncounter <49845522+penguinencounter@users.noreply.github.com>
2023-09-13 08:27:07 -0700

    More KotlinHelper

--- multitask/src/main/java/dev/aether/collaborative_multitasking/KotlinHelper.java
+++ multitask/src/main/java/dev/aether/collaborative_multitasking/KotlinHelper.java
@@ -1,12 +1,21 @@
 package dev.aether.collaborative_multitasking;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
 import kotlin.Unit;
 import kotlin.jvm.functions.Function0;
 import kotlin.jvm.functions.Function1;
+import kotlin.jvm.functions.Function2;
 
 public class KotlinHelper {
+    public static <A, B> Function2<A, B, Unit> kbu(BiConsumer<A, B> consumer) {
+        return (a, b) -> {
+            consumer.accept(a, b);
+            return Unit.INSTANCE;
+        };
+    }
+
     public static <T> Function1<T, Unit> kbu(Consumer<T> consumer) {
         return (t) -> {
             consumer.accept(t);
