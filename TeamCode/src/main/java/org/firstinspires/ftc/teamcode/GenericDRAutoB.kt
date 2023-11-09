313b7c9eb668f9e3819fc966e95dcb70392a78c5
penguinencounter <49845522+penguinencounter@users.noreply.github.com>
2023-11-09 15:20:59 -0800

    I Forgor to commit D:

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenericDRAutoB.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GenericDRAutoB.kt
@@ -1,24 +1,16 @@
 package org.firstinspires.ftc.teamcode
 
-import com.qualcomm.robotcore.hardware.IMU
 import dev.aether.collaborative_multitasking.MultitaskScheduler
 import dev.aether.collaborative_multitasking.Task
 import dev.aether.collaborative_multitasking.ext.maxDuration
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit
 import org.firstinspires.ftc.teamcode.abstractions.Claw
 import org.firstinspires.ftc.teamcode.configurations.RobotConfiguration
-import org.firstinspires.ftc.teamcode.odo.RotationUnit
-import org.firstinspires.ftc.teamcode.odo.degrees
 import org.firstinspires.ftc.teamcode.utility.MotorPowers
 
-/**
- * @param imu IMU in degrees, reset to 0
- */
 class GenericDRAutoB(
     private val robotConfig: RobotConfiguration,
     private val scheduler: MultitaskScheduler,
     private val claw: Claw,
-    private val imu: IMU,
 ) {
     // TODO: INCOMPLETE
     private val driveMotors = robotConfig.driveMotors()
@@ -29,26 +21,6 @@ class GenericDRAutoB(
 
         const val TurnSpeed = 0.25
         const val TurnDuration = 750 // ms
-    }
-
-    private fun getHeading(): RotationUnit {
-        return imu.robotYawPitchRollAngles.getYaw(AngleUnit.DEGREES).degrees
-    }
-
-    private fun doA180(): Task {
-        return scheduler.task {
-            // Placeholder values...
-            var startHeading: RotationUnit = 0.degrees
-            var targetHeading: RotationUnit = 180.degrees
-            var needFlip = false
-            +robotConfig.driveMotorLock
-            onStart { ->
-                startHeading = getHeading()
-                targetHeading = (startHeading + targetHeading)
-                // FIXME: if this ends up near 180 we can totally pass by it
-                needFlip = targetHeading > 180.degrees
-            }
-        }
     }
 
     private fun forwardPart(): Task {
